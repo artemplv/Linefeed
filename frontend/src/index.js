bb0710da9a1d31d8de8a73a31da9f82804ae15b2
artemplv <artemplv@gmail.com>
2023-07-21 12:05:02 -0400

    add react strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
-// eslint-disable-next-line react/jsx-filename-extension
-root.render(<App />);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
