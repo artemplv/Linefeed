99a8bb7eed7641dc5363587738660aaf6f83683f
artemplv <artemplv@gmail.com>
2023-07-21 17:42:23 -0400

    add initializer to transform request params keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::API
-  before_action :snake_case_params
-
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
@@ -19,11 +17,5 @@ class ApplicationController < ActionController::API
     if !current_user
       render json: { message: 'Unauthorized' }, status: :unauthorized
     end
-  end
-
-  private
-
-  def snake_case_params
-    params.deep_transform_keys!(&:underscore)
   end
 end
