eefff44483aac1843fd57b78767ece92403b125c
artemplv <artemplv@gmail.com>
2023-07-24 17:31:08 -0400

    fix config for deployment

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,90 +1,103 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 require('dotenv').config();
 
-const config = {
-  entry: [
-    path.resolve(__dirname, 'src', 'index.js'),
-  ],
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name].js',
-  },
-  devServer: {
-    port: 3000,
-    watchFiles: './src',
-    historyApiFallback: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        router: () => 'http://localhost:5000',
-        logLevel: 'debug',
-      },
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?[ac]ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { url: false },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-      {
-        test: /\.svg/,
-        type: 'asset/inline',
-      },
+const devBundleOutput = path.join(__dirname, 'build');
+const prodBundleOutput = path.join(__dirname, '../public');
+
+function getConfig(isProd) {
+  return {
+    entry: [
+      path.resolve(__dirname, 'src', 'index.js'),
     ],
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    extensions: ['*', '.js', '.jsx', '.json'],
-    alias: {
-      store: path.resolve(__dirname, 'src/store'),
-      api: path.resolve(__dirname, 'src/api'),
-      utils: path.resolve(__dirname, 'src/utils'),
-      components: path.resolve(__dirname, 'src/components'),
-      assets: path.resolve(__dirname, 'src/assets'),
+    output: {
+      path: isProd ? prodBundleOutput : devBundleOutput,
+      filename: '[name].js',
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new MiniCssExtractPlugin(),
-    new webpack.DefinePlugin({
-      // 'process.env.variable': JSON.stringify(process.env.variable),
-    }),
-  ],
-};
+    devServer: {
+      port: 3000,
+      watchFiles: './src',
+      historyApiFallback: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          router: () => 'http://localhost:5000',
+          logLevel: isProd ? 'silent' : 'debug',
+        },
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: ['babel-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?[ac]ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { url: false },
+            },
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+        {
+          test: /\.svg/,
+          type: 'asset/inline',
+        },
+      ],
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      extensions: ['*', '.js', '.jsx', '.json'],
+      alias: {
+        store: path.resolve(__dirname, 'src/store'),
+        api: path.resolve(__dirname, 'src/api'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        components: path.resolve(__dirname, 'src/components'),
+        assets: path.resolve(__dirname, 'src/assets'),
+      },
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        // 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new MiniCssExtractPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: './public/manifest.json',
+            to: isProd ? prodBundleOutput : devBundleOutput,
+          },
+        ],
+      }),
+    ],
+    devtool: isProd ? 'source-map' : 'eval-source-map',
+  };
+}
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'production') {
-    config.devtool = 'source-map';
-  } else {
-    config.devtool = 'eval-source-map';
-  }
+  const isProd = argv.mode === 'production';
+
+  const config = getConfig(isProd);
 
   return config;
 };
