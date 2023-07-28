d22d5740e594e779edf8f8c5716145d1e6cdad4a
artemplv <artemplv@gmail.com>
2023-07-28 14:23:21 -0400

    add workspace create controller

--- app/controllers/api/workspaces_controller.rb
+++ app/controllers/api/workspaces_controller.rb
@@ -12,6 +12,23 @@ class Api::WorkspacesController < ApplicationController
     render :show
   end
 
+  def create
+    @workspace = Workspace.new(workspace_params)
+    @workspace.name ||= 'My Workspace'
+    @workspace.owner_id = current_user.id
+
+    ActiveRecord::Base.transaction do
+      @workspace.save!
+      # add owner as a member of a workspace
+      WorkspaceUser.create!({
+        workspace_id: @workspace.id,
+        user_id: @workspace.owner_id,
+      });
+    end
+
+    render :show
+  end
+
   def update
     @workspace = Workspace.find(params[:id])
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
-    resources :workspaces, only: [:index, :show, :update]
+    resources :workspaces, only: [:index, :show, :update, :create]
   end
 
   get '*path', to: "static_pages#frontend_index"
