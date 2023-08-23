4f50be26ac908a7da534666ea5f3065389b48082
artemplv <artemplv@gmail.com>
2023-08-23 15:37:29 -0400

    update models for direct messages

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,31 @@
+# == Schema Information
+#
+# Table name: chats
+#
+#  id                :bigint           not null, primary key
+#  interlocutor_1_id :bigint           not null
+#  interlocutor_2_id :bigint           not null
+#  workspace_id      :bigint           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 class Chat < ApplicationRecord
+  validates :workspace_id, :interlocutor_1_id, :interlocutor_2_id,
+    presence: true
+  
+  validates_uniqueness_of :interlocutor_1_id, :scope => [:interlocutor_2_id]
+
+  belongs_to :interlocutor_1,
+    class_name: 'User'
+
+  belongs_to :interlocutor_2,
+    class_name: 'User'
+
+  belongs_to :workspace,
+    class_name: 'Workspace'
+
+  has_many :chat_messages,
+    dependent: :destroy
+  has_many :messages,
+    through: :chat_messages
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id           :bigint           not null, primary key
+#  body         :text             not null
+#  author_id    :bigint           not null
+#  workspace_id :bigint           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Message < ApplicationRecord
   validates :body, :workspace_id, :author_id,
     presence: true
@@ -13,4 +24,10 @@ class Message < ApplicationRecord
 
   has_one :channel,
     through: :channel_message
+
+  has_one :chat_message,
+    dependent: :destroy
+
+  has_one :chat,
+    through: :chat_message
 end
