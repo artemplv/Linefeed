8a4e7aa1daccd88de7fec8046cca0abe0afa0051
artemplv <artemplv@gmail.com>
2023-09-03 20:55:43 -0400

    fix icons for safari

--- frontend/src/components/shared/Icon/Icon.jsx
+++ frontend/src/components/shared/Icon/Icon.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import sprite from 'assets/icons/feather-sprite.svg';
+import * as Feather from 'react-feather';
+
+import { pascalCase } from 'change-case';
 
 function Icon(props) {
   const {
@@ -9,17 +11,21 @@ function Icon(props) {
     className,
   } = props;
 
+  const FeatherIcon = Feather[pascalCase(name)];
+
+  if (!FeatherIcon) {
+    return null;
+  }
+
   return (
     <i className={`svg-icon ${className}`}>
-      <svg
+      <FeatherIcon
         fill="none"
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
-      >
-        <use href={`${sprite}#${name}`} />
-      </svg>
+      />
     </i>
   );
 }
